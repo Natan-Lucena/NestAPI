78147e831ea111f13935cffdfb39713e9f744547
Natan <zerogamerptbr@gmail.com>
2023-12-22 02:04:58 -0300

    :sparkles: feat: authModule and everything it needs to work

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Authservice } from './auth.service';
+import { AuthDTO } from './dtos';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: Authservice) {}
   @Post('signup')
-  signUp() {
-    return 'im signed up';
+  signUp(@Body() dto: AuthDTO) {
+    return this.authService.singUp(dto);
   }
   @Post('signin')
   singIn() {
-    return 'im signed in';
+    return this.authService.signIn;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDTO } from './dtos';
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class Authservice {
-  login() {}
-  singup() {}
+  constructor(private prisma: PrismaService) {}
+
+  async singUp({ email, password }: AuthDTO) {
+    const hash = await argon.hash(password);
+    const user = await this.prisma.user.create({
+      data: { email, hash },
+    });
+    delete user.hash;
+    return user;
+  }
+  signIn() {}
 }
