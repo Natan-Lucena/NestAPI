fd7407932f89fe35202c8702988b213408936502
Natan <zerogamerptbr@gmail.com>
2023-12-27 09:59:29 -0300

    :lock ::Add HTTP status code to signin endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { Authservice } from './auth.service';
 import { AuthDTO } from './dtos';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: Authservice) {}
+
   @Post('signup')
   signUp(@Body() dto: AuthDTO) {
     return this.authService.singUp(dto);
   }
+
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   singIn(@Body() dto: AuthDTO) {
     return this.authService.signIn(dto);
