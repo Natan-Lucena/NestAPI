8b62528d634977c32251fe19d934cf54b30e8484
Natan <zerogamerptbr@gmail.com>
2023-12-28 01:05:27 -0300

    :test_tube: tests: the structure of all tests(needing implemetantion)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from './../src/prisma/prisma.service';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -17,9 +19,29 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
   afterAll(() => {
     app.close();
   });
-  it.todo('should pass');
+  describe('Auth', () => {
+    describe('SignUp', () => {
+      it.todo('Should sign up');
+    });
+    describe('SignIn', () => {
+      it.todo('Should sign in');
+    });
+  });
+  describe('User', () => {
+    describe('Get me', () => {});
+    describe('Edit user', () => {});
+  });
+  describe('Bookmarks', () => {
+    describe('Create Bookmarks', () => {});
+    describe('Get Bookmarks', () => {});
+    describe('Edit BookMarks', () => {});
+    describe('Get Bookmarks by id', () => {});
+    describe('Delete Bookmarks', () => {});
+  });
 });
